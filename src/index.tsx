47b6b7a87331512780ca903411b82bde570e7689
Alex Alan Nunes <alexalannunes@gmail.com>
2021-11-24 14:04:37 -0300

    feat: implement provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux'
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* <Provider store={{}}> */}
     <App />
+    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
