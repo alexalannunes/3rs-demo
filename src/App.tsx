5574c3727e614ea76a7191b67ce2045629962671
Alex Alan Nunes <alexalannunes@gmail.com>
2021-11-24 15:14:57 -0300

    feat: implements

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { Cart as CartBad } from "./1-readable/bad";
+import { Cart as CartGood } from "./1-readable/good";
+
+const CartComponent = {
+  bad: CartBad,
+  good: CartGood,
+};
+
+const Cart = CartComponent["good"];
+
 function App() {
   return (
     <div className="App">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea aliquid soluta
-      odit, excepturi accusantium culpa aliquam nulla fuga dolorum optio libero.
-      Aliquid quasi porro necessitatibus consectetur magnam similique non
-      maxime?
-      <h3>deldew</h3>
+      <h1>Readability - Reusability - Refactorability</h1>
+      <hr />
+      <Cart />
     </div>
   );
 }
