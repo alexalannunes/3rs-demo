eb3876f6e1362946ac4c2d7af9589dd5cf3514c5
Alex Alan Nunes <alexalannunes@gmail.com>
2021-12-13 15:45:04 -0300

    remove unused

--- src/3-refactorable/good/CartTable.tsx
+++ src/3-refactorable/good/CartTable.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { selectCart } from "./store/cart/cartSlice";
 import { selectCurrency } from "./store/cart/currencySlice";
